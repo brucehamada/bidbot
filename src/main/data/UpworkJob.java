3478d707c239f29df60b0ed76e05ed99daeb3228
Bruce <149814380+brucehamada@users.noreply.github.com>
2023-11-30 05:21:58 +0900

    small-content

--- src/main/data/UpworkJob.java
+++ src/main/data/UpworkJob.java
@@ -7,15 +7,19 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+
 public class UpworkJob {
-    private String title="React Native/Expo pro needed";
+    private String title = "React Native/Expo pro needed";
+
     public String getTitle() {
         return this.title = title;
     }
-    public void setTitle(String title){
+
+    public void setTitle(String title) {
         this.title = title;
     }
-//    @Override
+
+    //    @Override
 //    public String toString () {
 //        return "{ \"title\":\"" + title + "\", "
 //                + "\"pubtime\":\"" + pubtime + "\", "
@@ -26,31 +30,32 @@ public class UpworkJob {
 //                "\"subcategory\":\"" + subcategory + "\"," +
 //                " }";
 //    }
-    public static void printJob(UpworkJob upworkJob){
+    public static void printJob(UpworkJob upworkJob) {
         System.out.println("Title" + " : " + upworkJob.getTitle());
     }
-    public static void main(String[] args){
-        String url = "https://www.upwork.com/freelance-jobs/apply/Convert-Scan-CAD-Model-Solidworks-with-Source-file_~01afa7c794e7770eb0/";
-        try{
-            Connection.Response response = Jsoup.connect(url)
+
+    public static void main(String[] args) {
+        Document doc;
+        try {
+            doc = Jsoup
+                    .connect("https://www.upwork.com/freelance-jobs/apply/Rust-Developer-for-Windows-bit-Executable-with-OpenCV-Integration_~01219e84a60010687f/")
                     .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
-                    .referrer("https://www.google.com") //Set a valid referrer
-                    .followRedirects(true)
-                    .ignoreHttpErrors(true)
-                    .execute();
-            if(response.statusCode() == 200){
-                Document doc =response.parse();
-                //Process the document as needed
-                UpworkJob upworkJob = new UpworkJob();
-                System.out.println(doc.select("div.container"));
-                Elements jobs= doc.select("div.container");
-                upworkJob.setTitle(jobs.select("span.text-base").text());
-                printJob(upworkJob);
-            } else {
-                System.out.println("Error: " + response.statusCode());
-                // error:403 occured.
-            }
-        }catch(IOException e){
+                    .header("Accept-Language", "*")
+                    .get();
+//            if(response.statusCode() == 200){
+//                Document doc =response.parse();
+            //Process the document as needed
+//                UpworkJob upworkJob = new UpworkJob();
+//                Elements jobs= doc.select("div.visitor-page-container");
+//                upworkJob.setTitle(jobs.select("h4.m-0").text());
+//                printJob(upworkJob);
+                   System.out.println(doc);
+//            }
+//            else {
+//                System.out.println("Error: " + response.statusCode());
+//                // error:403 occured.
+//            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
 //        Elements jobs= doc.select("div.container");
